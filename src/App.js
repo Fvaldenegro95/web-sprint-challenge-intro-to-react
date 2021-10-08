b370fec9950e3c88b51a0cf65a3c23481a830021
Fvaldenegro95 <fvaldenegro95@gmail.com>
2021-10-08 14:54:44 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import styled from "styled-components";
+import Character from './components/Character.js'
+import axios from 'axios';
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +13,30 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [character, setCharacter] = useState([]);
+
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(res =>{
+      setCharacter(res.data);
+    })
+    .catch(err =>{
+      console.error(err)
+    })
+  }, [])
+
+
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+
+      <h1 className="Header">Star Wars Characters</h1>
+      {character.map(character => {
+        return <Character key = {character.id} info = {character}/>
+      })
+      }
+
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,55 @@
-// Write your Character component here
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+
+const TitleStyle = styled.h1`
+font-family: 'Bebas Neue', cursive;
+border: 2px dashed white;
+width: 40%;
+font-size: 4em;
+margin-left: 30%;
+text-align: center;
+color: white;
+`
+
+const BigStyle = styled.div`
+font-family: 'Bebas Neue', cursive;
+padding: 2rem;
+`
+const InfoStyle = styled.div`
+    border: 2px dashed white;
+    width: 20%;
+    margin-left: 40%;
+    color: white;
+`
+
+
+
+export default function Character({info}){
+    const [visible, setVisible] = React.useState(false);
+
+    return (
+
+            <BigStyle>
+            
+            <TitleStyle onClick={() => setVisible(!visible)}>
+                {info.name}
+                {visible ? '' : ''}          
+
+            </TitleStyle>
+            <InfoStyle>
+            {visible && <div>
+            <p>Gender: {info.gender}</p>
+            <p>Height: {info.height}</p>
+            <p>Mass: {info.mass}</p>
+            <p>Birth Year: {info.birth_year}</p>
+            <p>Eye Color: {info.eye_color}</p>
+            <p>Hair Color: {info.hair_color}</p>
+            <p>Skin Color: {info.skin_color}</p>
+            </div>}
+            </InfoStyle>
+
+        </BigStyle>
+
+    )
+}
